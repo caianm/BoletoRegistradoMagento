f8c9610966f6dee4421990d62f3d64d1cceb445b
Caian Monteiro <caianmonteiro@hotmail.com>
2018-12-05 08:58:15 -0200

    First commit - creating base folders

---
+++ app/code/community/CaianMonteiro/BoletoRegistrado/Helper/Data.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: caian
+ * Date: 05/12/2018
+ * Time: 08:26
+ */ 
+class CaianMonteiro_BoletoRegistrado_Helper_Data extends Mage_Core_Helper_Abstract {
+
+}
---
+++ app/code/community/CaianMonteiro/BoletoRegistrado/sql/caianmonteiro_boletoregistrado_setup/install-0.1.0.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: caian
+ * Date: 05/12/2018
+ * Time: 08:26
+ */ 
+/* @var $installer Mage_Core_Model_Resource_Setup */
+$installer = $this;
+
+$installer->startSetup();
+
+
+
+$installer->endSetup();
